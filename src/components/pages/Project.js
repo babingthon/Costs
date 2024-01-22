bcc9cd3e359727e1a6313f44dac1e8d79ccd2ab6
babingthon <babingthon.bandeira@gmail.com>
2024-01-22 15:36:19 -0300

    .

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -9,16 +9,32 @@ import ProjectForm from "../project/ProjectForm";
 
 function Project() {
   const { id } = useParams();
-  const [projectData, setProjectData] = useState([]);
+  const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
 
   useEffect(() => {
-    async function fetchData() {
+    
+    const data = () => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((resp) => resp.json())
+      .then((data) => setProject(data))
+      .catch((err) => alert(err.message));
+    
+      console.log(project);
+    }
+    
+    data();
+    /*async function fetchData() {
       let request = await api.loadProject(id);
       setProjectData(request.data.project);
     }
     fetchData();
-    console.log(projectData)
+    console.log(projectData)*/
   }, [id]);
 
   const toggleProjectForm = () => {
@@ -35,24 +51,24 @@ function Project() {
 
   return (
     <>
-      {projectData.project.name ? (
+      {project.project.name ? (
         <div className={styles.project_details}>
           <Container customClass="column">
             <div className={styles.details_container}>
-              <h1>Projeto: {projectData.project.name}</h1>
+              <h1>Projeto: {project.project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
                 {!showProjectForm ? "Editar projeto" : "fechar projeto"}
               </button>
               {!showProjectForm ? (
                 <div className={styles.project_info}>
                   <p>
-                    <span>Categoria:</span> {projectData.category.name}
+                    <span>Categoria:</span> {project.project.category.name}
                   </p>
                   <p>
-                    <span>Total de Orçamento:</span> R${projectData.budget}
+                    <span>Total de Orçamento:</span> R${project.project.budget}
                   </p>
                   <p>
-                    <span>Total Utilizado:</span> R${projectData.cost}
+                    <span>Total Utilizado:</span> R${project.project.cost}
                   </p>
                 </div>
               ) : (
@@ -60,7 +76,7 @@ function Project() {
                   <ProjectForm
                     handleSubmit={editPost}
                     btnText="Concluir edição"
-                    projectData={projectData}
+                    projectData={project}
                   />
                 </div>
               )}
@@ -71,6 +87,7 @@ function Project() {
         <Loading />
       )}
     </>
+      
   );
 }
 
